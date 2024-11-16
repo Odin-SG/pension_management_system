00f47a8593261d3924740e7ab0474c48f5a84570
Odin-SG <yuga1994@mail.ru>
2024-11-16 10:38:02 +0300

    Вынес всё в отдельный JS

--- app/static/js/scripts.js
+++ app/static/js/scripts.js
@@ -1,1 +1,69 @@
-$(document).ready(function() {}
+$(document).ready(function() {
+    var editModal = $('#edit-user-modal');
+    var closeButton = $('.close-button');
+    var contributeModal = $('#contribute-modal');
+    var withdrawModal = $('#withdraw-modal');
+    var contributeButton = $('#contribute-button');
+    var withdrawButton = $('#withdraw-button');
+    var withdrawCloseButton = $('.withdraw-close-button');
+
+    $('.edit-user-button').on('click', function() {
+        var userId = $(this).data('user-id');
+        $('#edit-user-id').val(userId);
+        editModal.show();
+    });
+
+    closeButton.on('click', function() {
+        editModal.hide();
+    });
+
+    $(window).on('click', function(event) {
+        if ($(event.target).is(editModal)) {
+            editModal.hide();
+        }
+    });
+
+    // Получение информации о конкретном пользователе
+    $('.view-user-button').on('click', function() {
+        var userId = $(this).data('user-id');
+        $.get("/admin_panel", { user_id: userId }, function(data) {
+            if (data.specific_user) {
+                $('#user-details').show();
+                $('#user-username').text(data.specific_user.username);
+                $('#user-role').text(data.specific_user.role);
+                $('#user-total-amount').text(data.specific_user.total_amount);
+
+                var historyHtml = '';
+                data.user_history.forEach(function(entry) {
+                    historyHtml += '<li>Сумма: ' + entry.amount + ' руб., Дата: ' + entry.contribution_date + '</li>';
+                });
+                $('#user-history').html(historyHtml);
+            }
+        });
+    });
+
+    contributeButton.on('click', function() {
+        contributeModal.show();
+    });
+
+    withdrawButton.on('click', function() {
+        withdrawModal.show();
+    });
+
+    closeButton.on('click', function() {
+        contributeModal.hide();
+    });
+
+    withdrawCloseButton.on('click', function() {
+        withdrawModal.hide();
+    });
+
+    $(window).on('click', function(event) {
+        if ($(event.target).is(contributeModal)) {
+            contributeModal.hide();
+        }
+        if ($(event.target).is(withdrawModal)) {
+            withdrawModal.hide();
+        }
+    });
+});
