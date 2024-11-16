08bcf60c6a56564b083b385595de66d28cbb1813
Odin-SG <yuga1994@mail.ru>
2024-11-16 11:28:35 +0300

    Обновлена админка. Фронт и отображение

--- app/static/js/scripts.js
+++ app/static/js/scripts.js
@@ -66,4 +66,69 @@ $(document).ready(function() {
             withdrawModal.hide();
         }
     });
+
+    var userDetailsModal = $('#user-details-modal');
+    var closeDetailsButton = $('.details-close-button');
+
+    $('.view-user-button').on('click', function() {
+        var userId = $(this).data('user-id');
+        $.get("/admin_panel", { user_id: userId }, function(data) {
+            if (data.specific_user) {
+                $('#modal-user-username').text(data.specific_user.username);
+                $('#modal-user-role').text(data.specific_user.role);
+                $('#modal-user-total-amount').text(data.specific_user.total_amount);
+
+                var historyHtml = '';
+                data.user_history.forEach(function(entry) {
+                    historyHtml += '<li>Сумма: ' + entry.amount + ' руб., Дата: ' + entry.contribution_date + '</li>';
+                });
+                $('#modal-user-history').html(historyHtml);
+
+                userDetailsModal.show();
+            }
+        });
+    });
+
+    closeDetailsButton.on('click', function() {
+        userDetailsModal.hide();
+    });
+
+    $(window).on('click', function(event) {
+        if ($(event.target).is(userDetailsModal)) {
+            userDetailsModal.hide();
+        }
+    });
+
+    // Асинхронное редактирование пользователя
+    $('#edit-user-form').on('submit', function(event) {
+        event.preventDefault(); // Останавливаем стандартное поведение формы
+
+        var formData = {
+            user_id: $('#edit-user-id').val(),
+            new_username: $('#new-username').val(),
+            new_role: $('#new-role').val(),
+            new_amount: $('#new-amount').val()
+        };
+
+        fetch('/admin_panel', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(formData)
+        })
+        .then(response => response.json())
+        .then(data => {
+            if (data.success) {
+                alert('Пользователь успешно обновлён!');
+                $('#edit-user-modal').hide();
+                location.reload(); // Обновляем таблицу
+            } else {
+                alert('Ошибка при обновлении пользователя: ' + data.error);
+            }
+        })
+        .catch(error => {
+            console.error('Ошибка:', error);
+        });
+    });
 });
